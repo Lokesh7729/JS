07396494ad0e2ef0c1cb03507dddb135ab08d1eb
Lokesh7729 <lokeshchauhan7729@gmail.com>
2023-11-30 13:50:26 +0530

    while do while loops

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -1,9 +1,35 @@
-let  a = 10 ;
-let b= 10;
-if (a>b){
-    console.log("a is greater than b");
-} else if(a <b){
-    console.log("a is less than b");
-}else{
-    console.log("a = b");
-}
+// let  a = 10 ;
+// let b= 10;
+// if (a>b){
+//     console.log("a is greater than b");
+// } else if(a <b){
+//     console.log("a is less than b");
+// }else{
+//     console.log("a = b");
+// }
+
+
+// for loop
+
+// for(initailExxpression; condition ; incrementExpression){}
+// for (let i = 1; i <=5; i++) {
+//     console.log("Hello There" , i);
+    
+// }
+
+
+// for (let i = 1; i <=1000 ; i++) {
+//     console.log("lokesh");
+    
+// }
+
+
+// While loop 
+// let i = 20 
+// do while loop
+
+// do {
+//     console.log("lokesh");
+//     i++;
+// } while (i<=400);
+
