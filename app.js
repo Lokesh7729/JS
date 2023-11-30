2dfbeba623aed6f0617ce23533610fbd33656e8d
Lokesh7729 <lokeshchauhan7729@gmail.com>
2023-11-30 11:33:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// let name = "Lokesh";
+// let whatDoYouWannaBecomeInYourLife= "programmer";
+// let gender = "Male";
+// let twitterHandle = "lokesh7729"
+
+// console.log(name);
+// console.log(whatDoYouWannaBecomeInYourLife);
+// console.log(gender);
+// console.log(twitterHandle);
+
