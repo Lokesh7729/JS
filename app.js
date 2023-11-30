90ab3911eadddc1ac8f16d6c8f5c93642825677d
Lokesh7729 <lokeshchauhan7729@gmail.com>
2023-11-30 11:55:30 +0530

    comparison Operators

--- app.js
+++ app.js
@@ -8,3 +8,61 @@
 // console.log(gender);
 // console.log(twitterHandle);
 
+
+// Falsy value in js
+//  -> false
+//  -> null
+//  -> undefined
+//  -> 0 
+//  -> -0
+//  -> NaN
+//  -> '',"",``, (empty quotes)
+
+
+// Challenge 
+// let isJsProgrammingLanguage = true;
+// let isJsHard = false;
+// let favNumb = 13 ;
+
+// console.log(isJsProgrammingLanguage);
+// console.log(isJsHard);
+// console.log(favNumb + undefined);
+
+
+// Comparison Operators
+// -----------------------------------------
+// -> Relational Operator 
+
+// > grater then
+// < less then
+// >= greater then or equal to
+// <= less then or equal to
+
+// console.log(10>10);  // false
+// console.log(10<10); // false
+// console.log(10<=10);// true
+// console.log(10>=10);// true
+// ----------------------------------------
+// -> Equality Operator 
+
+// === strict equality (type + value)
+// ==! strict non-equality operators (type + value)
+// == lose equality operator (value)
+// =! lose not equality operator (value)
+
+// ----------------------------------------------
+
+// let favNumb = 13;
+// let favNumb2= 13;
+// console.log(favNumb>favNumb2);
+// console.log(favNumb<favNumb2);
+// console.log(favNumb>=favNumb2);
+// console.log(favNumb<=favNumb2);
+
+// console.log(favNumb===favNumb2);
+// console.log(favNumb==!favNumb2);
+// console.log(favNumb==favNumb2);
+// console.log(favNumb=!favNumb2);
+
+
+
