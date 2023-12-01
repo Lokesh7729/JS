d2eeec095049c11e6c1e9a92980df2245c47c8c4
Lokesh7729 <lokeshchauhan7729@gmail.com>
2023-12-02 01:00:17 +0530

    arrays

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -33,3 +33,43 @@
 //     i++;
 // } while (i<=400);
 
+
+
+// Arrays -> 0 index based
+let arr=['eat', 'sleep', 'code','repeat'];
+
+// Accessing items from our array
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+
+
+// Nested Array // 2d array
+// const nestArr=['one',['two','three'],'four',true];
+// console.log(nestArr[1][1]);
+
+// const letters=['h','e','l','l','0']
+// console.log(letters);
+
+
+// challange
+
+// let favSinger=['sanam','atif aslam', 'bella']
+// console.log(favSinger[0]);
+
+// let favNumb=[13,45,69,80]; /
+ 
+//
+
+//ARRAYS
+
+console.log(arr);
+arr.push("rest");
+
+console.log(arr);
+arr.shift();
+console.log(arr);
+arr.unshift("banana");
+console.log(arr);
+
