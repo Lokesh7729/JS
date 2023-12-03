686e2b9d6e0c3be4770e48122021a2bedceff16a
Lokesh7729 <lokeshchauhan7729@gmail.com>
2023-12-03 16:31:12 +0530

    function

---
+++ fucntion/fucntion.js
@@ -0,0 +1,16 @@
+function greet(){
+    console.log("hello good morning");
+}
+greet()
+
+function sayHello(name){
+console.log(`hello ${name}`);
+}
+sayHello("lokesh")
+
+function add(num1,num2){
+    return num1+num2
+
+}
+res=add(10,20);
+console.log(res);
