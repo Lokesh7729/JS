69c734a4a24e8d6b731e809240f96c62d53b804e
Lokesh7729 <lokeshchauhan7729@gmail.com>
2023-12-03 17:26:26 +0530

    function

--- fucntion/fucntion.js
+++ fucntion/fucntion.js
@@ -1,16 +1,50 @@
-function greet(){
-    console.log("hello good morning");
-}
-greet()
+// function greet(){
+//     console.log("hello good morning");
+// }
+// greet()
 
-function sayHello(name){
-console.log(`hello ${name}`);
+// function sayHello(name){
+// console.log(`hello ${name}`);
+// }
+// sayHello("lokesh")
+
+// function add(num1,num2){
+//     return num1+num2
+
+// }
+// res=add(10,20);
+// console.log(res);
+
+// // challange
+
+// function myfunction(a ,b ){
+//     return a*b
+// }
+// res= myfunction(13,10);
+// console.log(res);
+
+// Function Declaration
+function sayHello(username){
+    console.log(`hello ${username}`);
 }
 sayHello("lokesh")
 
-function add(num1,num2){
-    return num1+num2
-
+// Function Expression
+const greet = function (user){
+    console.log(`HELLO ${user}`);
 }
-res=add(10,20);
-console.log(res);
+greet("loki")
+
+
+// Call back function => When we provide fucntion as an (agrument) to other function that fucntion is known as callback funciton
+
+//practice
+
+function showCallFunc(fn){
+    const a = "helloo";
+    fn(a);
+}
+
+showCallFunc(function(a){
+    console.log(a);
+})
